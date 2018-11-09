d1cdf464b0e756a5720fb296bbfe01520236a6a8
DED8IRD <ewu72829@gmail.com>
2018-11-09 15:19:20 -0800

    display todos array in jsx using map

--- what-todo-app/src/components/App.js
+++ what-todo-app/src/components/App.js
@@ -1,30 +1,30 @@
 import React from 'react'
-import List from '../playground/RenderArraysAsList'
+// import List from '../playground/RenderArraysAsList'
 
 export default class App extends React.Component {
-  // render() {
-  //   let app = {
-  //     title: 'WhatTodo',
-  //     subtitle: 'Decide what to do next.',
-  //     todos: ['wash cat', 'burn mail', 'terrorize neighbor']    
-  //   }
+  render() {
+    let app = {
+      title: 'WhatTodo',
+      subtitle: 'Decide what to do next.',
+      todos: ['wash cat', 'burn mail', 'terrorize neighbor']    
+    }
 
-  //   return (
-  //     <div>
-  //       <h1>{app.title}</h1>
-  //       {app.subtitle && <h3>{app.subtitle}</h3>}
-  //       <ul>
-  //         {(app.todos ? 
-  //           app.todos :
-  //           "Nothing to do."
-  //         )}
-  //       </ul>
-  //     </div>
-  //   )
-  // }
-
-  render () {
-    return <List />
+    return (
+      <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <h3>{app.subtitle}</h3>}
+        <ul>
+          {(app.todos ? 
+            app.todos.map(todo => <li key={todo}>{todo}</li>) :
+            "Nothing to do."
+          )}
+        </ul>
+      </div>
+    )
   }
+
+  // render () {
+  //   return <List />
+  // }
 }
 
