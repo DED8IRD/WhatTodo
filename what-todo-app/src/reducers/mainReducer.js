259d86a1d57bd1a8a80d0b4126b8f5a666ee85f5
DED8IRD <ewu72829@gmail.com>
2018-11-20 01:10:05 -0800

    combine reducers

---
+++ what-todo-app/src/reducers/mainReducer.js
@@ -0,0 +1,20 @@
+// mainReducer.js
+// Combines smaller reducers into single object. 
+// We no longer need to know a complete initial state.
+
+import todos from './todoReducer'
+import visibilityFilter from './visibilityFilterReducer'
+import {combineReducers} from 'redux'
+
+export default const todoApp = combineReducers({
+	todos,
+	visibilityFilter
+})
+
+// // The following lines do the same as the above
+// export default const todoApp = (state={}, action) => {
+// 	return {
+// 		todos: todos(state.todos, action),
+// 		visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+// 	}
+// }
