918578bb590a08badc360a3639afae0435c3c90a
DED8IRD <ewu72829@gmail.com>
2018-11-20 00:55:12 -0800

    reducer composition

---
+++ what-todo-app/src/reducers/todoReducer.js
@@ -0,0 +1,22 @@
+// todoReducer.js
+// reducer composition for updating todos state
+
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+} from '../actions/actions'
+
+export default const todos = (state=[], action) => {
+	switch (action.type) {
+		case ADD_TODO:
+			return [...state.todos, {text: action.text, completed: false}]
+		case TOGGLE_TODO:
+			return state.todos.map((todo, idx) => (
+				idx === action.idx 
+				?	{text: todo.text, completed: !todo.completed}) 
+				: todo
+			)
+		default:
+			return state
+	}
+}
---
+++ what-todo-app/src/reducers/visibilityFilterReducer.js
@@ -0,0 +1,17 @@
+// visibilityFilterReducer.js
+// reducer composition for updating todos state
+
+import {
+	VisibilityFilters, 
+	SET_VISIBILITY_FILTER
+} from '../actions/actions'
+
+const {SHOW_ALL} = VisibilityFilters
+export default const visibilityFilter = (state=SHOW_ALL, action) => {
+	switch (action.type) {
+		case SET_VISIBILITY_FILTER:
+			return action.filter
+		default:
+			return state
+	}
+}
