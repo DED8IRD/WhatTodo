58431b0ccdbd1d6b7345d09f207feaf6d5e48af4
DED8IRD <ewu72829@gmail.com>
2018-11-26 17:32:20 -0800

    pass store with <Provider>

--- what-todo-app/src/index.js
+++ what-todo-app/src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-
+import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import {
   addTodo,
   toggleTodo,
@@ -19,25 +10,20 @@ import {
   setVisibilityFilter,
   VisibilityFilters
 } from './actions/actions'
-import todoApp from './reducers/mainReducer'
+import todoApp from './reducers/rootReducer'
+import App from './components/App';
 
+// create redux store
 const store = createStore(todoApp)
+// pass store into root app via <Provider>
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
 
-// log the initial state
-console.log(store.getState())
-
-// log every state change
-// note: subscribe() returns a function for unregistering the listener
-// (similar to setInterval() returning id for clearInterval())
-const unsubscribe = store.subscribe(() => console.log(store.getState()))
-
-// dispatch actions 
-store.dispatch(addTodo('wash cat'))
-store.dispatch(addTodo('butter plates'))
-store.dispatch(addTodo('terrorize neighbor'))
-store.dispatch(toggleTodo('wash cat'))
-store.dispatch(toggleTodo('butter plates'))
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-
-// stop listening to state updates 
-unsubscribe()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
